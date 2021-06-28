f0fd37784df44c6849caf7919ad699e092430d4d
greyhub <luongnv.grey@gmail.com>
2021-06-28 13:18:42 +0700

    Server using a user-defined KeyFunc

---
+++ cookbook/jwt/user-defined-keyfunc/keyfunc.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"github.com/lestrrat-go/jwx/jwk"
+)
+
+func getKey(token *jwt.Token) (interface{}, error) {
+
+	keySet, err := jwk.Fetch(context.Background(), "https://www.googleapis.com/oauth2/v3/certs")
+	if err != nil {
+		return nil, err
+	}
+
+	keyID, ok := token.Header["kid"].(string)
+	if !ok {
+		return nil, errors.New("Expecting JWT header to have a key ID in the kid field")
+	}
+
+	key, found := keySet.LookupKeyID(keyID)
+	if !found {
+		return nil, fmt.Errorf("Unable to fin key %q", keyID)
+	}
+
+	var pubkey interface{}
+	if err := key.Raw(&pubkey); err != nil {
+		return nil, fmt.Errorf("Unable to get the public key. Error: %s", err.Error())
+	}
+
+	return pubkey, nil
+}
+
+func accessible(c echo.Context) error {
+	return c.String(http.StatusOK, "Accessible")
+}
+
+func restricted(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.String(http.StatusOK, "Welcome "+name+"!")
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", accessible)
+
+	r := e.Group("/restricted")
+	{
+		config := middleware.JWTConfig{
+			KeyFunc: getKey,
+		}
+		r.Use(middleware.JWTWithConfig(config))
+		r.GET("", restricted)
+	}
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
