d40e1632cb9d932bcbf77796d9d5af34dd4439a4
greyhub <luongnv.grey@gmail.com>
2021-07-20 13:22:31 +0700

    Create Google App Engine

---
+++ cookbook/gae/app-standalone.go
@@ -0,0 +1,24 @@
+// +build !appengine,!appenginevm
+
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func createMux() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+
+	e.Static("/", "public")
+
+	return e
+}
+
+func main() {
+	e.Logger.Fatal(e.Start(":8040"))
+}
